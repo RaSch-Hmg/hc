5168c98fa95898aa27fe4bbb98c270a2bc64ba46
Mr.Hmg <rasch@myhmg.de>
2017-12-08 13:02:09 +0100

    Create config.php

---
+++ config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+// Config
+$controlFile = "hc-config.xml";
+$ConfigParams = Array();
+$ConfigParams[0]['codeOn']='111236';
+$ConfigParams[0]['codeOff']='111236';
+$ConfigParams[0]['name']='Schalter - 1';
+
+$ConfigParams[1]['codeOn']='11123698';
+$ConfigParams[1]['codeOff']='11123698';
+$ConfigParams[1]['name']='Schalter - 2';
+
+
+
+
+?>

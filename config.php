dea5b7512a268f5007dd785c27069763bedf7aeb
Mr.Hmg <rasch@myhmg.de>
2017-12-11 12:39:46 +0100

    Update new

--- config.php
+++ config.php
@@ -4,15 +4,13 @@
 // Config
 $controlFile = "hc-config.xml";
 $ConfigParams = Array();
-$ConfigParams[0]['codeOn']='111236';
-$ConfigParams[0]['codeOff']='111236';
+
+// Steckdose 1
 $ConfigParams[0]['name']='Schalter - 1';
-
-$ConfigParams[1]['codeOn']='11123698';
-$ConfigParams[1]['codeOff']='11123698';
+$ConfigParams[0]['code']='11111 2';
+// Steckdose 2
 $ConfigParams[1]['name']='Schalter - 2';
-
-
-
+$ConfigParams[1]['code']='11111 2';
+//.....
 
 ?>

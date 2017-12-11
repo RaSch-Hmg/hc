dfc42d7017e45b28c164f881a1d8f76b09baf7e6
Mr.Hmg <rasch@myhmg.de>
2017-12-11 12:40:42 +0100

    update new Remote

--- hc-cron.php
+++ hc-cron.php
@@ -1,24 +1,32 @@
 <?php
 
+
+/*
+sudo visudo
+www-data ALL=NOPASSWD:/home/pi/433Utils/RPi_utils/codesend
+www-data ALL=NOPASSWD:/home/pi/raspberry-remote/send
+
+
+*/
+
+$shellEx ="sudo /home/pi/raspberry-remote/send ";
+
 require_once('config.php'); 
 
 if(file_exists($controlFile)){
-    
+
 	$xml = simplexml_load_file($controlFile);
 	foreach($xml->children() as $child) {
         $role = $child->attributes();
 		$state   = "";
-		$codeOn  = "";
-		$codeOff = "";
+		$code  = "";
 		$timeOn  = "";
 		$timeOff = "";	
 		foreach($child as $key => $value) {
 				if ($key=="state") 
 					$state = $value;
-				if ($key=="code-on") 
-					$codeOn = $value;
-				if ($key=="code-off") 
-					$codeOff = $value;
+				if ($key=="code") 
+					$code = $value;
 				if ($key=="time-on") 
 					$timeOn = $value;
 				if ($key=="time-off") 
@@ -38,15 +46,15 @@ if(file_exists($controlFile)){
 				$offDate  = $date->format('Y-m-d').' '.$timeOff.':00';
 			}
 			if ( (strtotime($aktDate) > strtotime($onDate)) and ( strtotime($offDate) > strtotime($aktDate)  ) )
-				echo $output = shell_exec('sudo /home/pi/433Utils/RPi_utils/codesend '.$codeOn);
+				$output = shell_exec($shellEx.$code." 1");
 			else
-				echo $output = shell_exec('sudo /home/pi/433Utils/RPi_utils/codesend '.$codeOff);	
+				$output = shell_exec($shellEx.$code." 0");	
 		}
 		elseif ( $state == 'on' )
-			echo $output = shell_exec('sudo /home/pi/433Utils/RPi_utils/codesend '.$codeOn);
+			$output = shell_exec($shellEx.$code." 1");
 		else
-			echo $output = shell_exec('sudo /home/pi/433Utils/RPi_utils/codesend '.$codeOff);
-	}
+			$output = shell_exec($shellEx.$code." 0");
+	}	
 }	
 ?>	
 	
